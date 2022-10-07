7974933c2498fe6b71a75dab254f83059c97de9b
oleg- <o.boys@yandex.ru>
2022-10-07 15:16:32 +0300

    Таблица до 10

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 11; i++){
+            for(int k = 1; k < 11; k++){
+                System.out.print(k * i + " ");
+            }
+            System.out.println("");
+        }
+    }
 }
